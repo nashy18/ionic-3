a6410be14af77dba6f8aac61d1d134684669f2f4
mahesh2528 <mahesh.devda@yahoo.com>
2018-05-06 19:54:21 +0530

    Add files via upload
    ionic 3 + Angular 5 base project

---
+++ src/pages/home/home.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { RestProvider } from '../../providers/rest/rest';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  countries: any;
+  errorMessage: string;
+
+  constructor(public navCtrl: NavController, public rest: RestProvider) {
+
+  }
+
+  ionViewDidLoad() {
+    this.getCountries();
+  }
+
+  getCountries() {
+    this.rest.getCountries()
+       .subscribe(
+         countries => this.countries = countries,
+         error =>  this.errorMessage = <any>error);
+  }
+}
