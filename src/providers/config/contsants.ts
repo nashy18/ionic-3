05e8fe901111926d89eaa657b1c76565613e6352
mahesh2528 <mahesh.devda@yahoo.com>
2018-06-10 21:36:49 +0530

    Added constant file and removed unused file

---
+++ src/providers/config/contsants.ts
@@ -0,0 +1,19 @@
+export const Global = Object.freeze({
+
+    "BaseAPIURL" :"http://localhost:1337/"
+    
+})
+
+
+export const Enums = Object.freeze({
+
+    
+})
+
+export const APIActions = Object.freeze({
+
+    "addVisitor": "visitor",
+    "getAllDepartmets": "department",
+    "getEmployeesByDepartment": "user/search",
+    "getAllPurposes": "visitPurpose"
+})
